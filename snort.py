e78f7d0ae112535fa57b04caeb0365cc8e49d434
Ian A. Mason <iam@csl.sri.com>
2017-07-12 07:00:23 -0700

    OK no to go after the 'sites implicated in'

--- snort.py
+++ snort.py
@@ -1,5 +1,6 @@
 #!/usr/bin/env python
 
+import re
 import urllib2
 
 #pip install beautifulsoup4
@@ -39,6 +40,13 @@ proteins = [
 url = "http://www.phosphosite.org/simpleSearchSubmitAction.action?queryId=-1&from=0&searchStr={0}"
 
 
+
+firstIdPattern = re.compile('id=(\d*)')
+
+# the first looks to be the one we want. but we could pattern match if that fails.
+# do the simple minded thing first.
+#<a class="link13HoverRed" href="/../proteinAction.action?id=2791&amp;showAllSites=true">human</a>
+#http://www.phosphosite.org/proteinAction.action?id=2791&showAllSites=true
 def firstLink(p):
     f =  urllib2.urlopen(url.format(p))
     html_doc = f.read()
@@ -47,9 +55,76 @@ def firstLink(p):
 
     #print(soup.prettify())
 
+    candidate = soup.find_all('a', class_='link13HoverRed')[0] #first element looks to be the one we want.
 
-    return soup.find_all('a', class_='link13HoverRed')[0]  #first element looks to be the one we want.
+    match = firstIdPattern.search(str(candidate))
+
+    if match:
+        retval = match.group(1)
+    else:
+        retval = None
+
+    return retval
+
 
 
 for p in proteins:
-    print p, '\t', firstLink(p)
+    print '# {0}\n{1},'.format(p, firstLink(p))
+
+#from which we deduce we get
+
+
+protein_ids = [
+    # AKT1
+    570,
+    # APC
+    3867,
+    # ATF1
+    3541,
+    # ATR
+    576,
+    # BIM
+    2791,
+    # BRAF
+    577,
+    # CHEK2
+    468,
+    # CREB1
+    1489,
+    # FAK1
+    598,
+    # GATA3
+    7977,
+    # HSPB1
+    989,
+    # IGFBP2
+    22867,
+    # IRS1
+    855,
+    # JUN
+    943,
+    # MTOR
+    564,
+    # PDCD4
+    4192,
+    # PLK1
+    648,
+    # PRAS40
+    22501100,
+    # RAF1
+    653,
+    # RASGRP1
+    7848,
+    # SETD8
+    13600,
+    # STAT1
+    1048,
+    # STAT3
+    1050,
+    # TP53BP1
+    3129,
+    # TTK
+    779,
+    # YBX1
+    2629,
+]
