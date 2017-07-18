de5cf71d0784f71357d0466506c6fe5f4268c5d4
Ian A. Mason <iam@csl.sri.com>
2017-07-18 15:37:54 -0700

    queries concocted

--- src/extract.py
+++ src/extract.py
@@ -43,15 +43,44 @@ def process_proteins(conn, db):
     dump_database(db, 'proteins_sql.json')
 
 
+def process_site_row(db, row):
+    pass
 
 def process_sites(conn, db):
-     pass
+    cursor = conn.cursor()
+    site_query = '''
+    SELECT s.site_pid, sc.name, sn.name, s.phosphosite_id
+    FROM site AS s, site_category AS sc, site_name AS sn
+    WHERE s.site_category_id = sc.site_category_id AND  s.site_name_id = sn.site_name_id
+    '''
+    cursor.execute(site_query)
+    while True:
+        row = cursor.fetchone()
+        if row is None:
+            break
+        process_site_row(row, db)
+    dump_database(db, 'sites_sql.json')
+
+def process_control_row(db, row):
+    pass
 
 
 def process_controls(conn, db):
-    pass
+    cursor = conn.cursor()
+    control_query = '''
+    SELECT c.phosphosite_id, cc.name, cn.name, c.controller
+    FROM control AS c, control_category AS cc, control_name AS cn
+    WHERE c.control_category_id = cc.control_category_id AND  c.control_name_id = cn.control_name_id
+    '''
+    cursor.execute(control_query)
+    while True:
+        row = cursor.fetchone()
+        if row is None:
+            break
+        process_control_row(row, db)
+    dump_database(db, 'controls_sql.json')
 
 
 process_proteins(conn, database_0)
 process_sites(conn, database_1)
-process_controls(conn, database_0)
+process_controls(conn, database_2)
