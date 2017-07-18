86003cdce2d10b4608b9e7e94fbeaa75e4822c0d
Ian A. Mason <iam@csl.sri.com>
2017-07-17 17:43:55 -0700

    got the second page data, just need to insert it.

--- src/snort_1.py
+++ src/snort_1.py
@@ -46,8 +46,6 @@ protein_list = [
     "YBX1",
 ]
 
-#'http://www.phosphosite.org/simpleSearchSubmitAction.action?queryId=-1&from=0&searchStr="AKT1"'
-
 conn = sqlite3.connect('../db/phosphosite.sqlite')
 
 
@@ -85,13 +83,17 @@ def get_protein_id(conn, name):
     #step three: update the table to reflect the status
     insert_query = 'INSERT INTO protein (name, phosphosite_id) VALUES(?)'
     if pid != -1:
-        cursor.execute('INSERT INTO protein (name, phosphosite_id) VALUES(?, ?)', (name, pid))
+        cursor.execute('INSERT INTO protein (name, phosphosite_id) VALUES(?, ?)', [name, pid])
     else:
-        cursor.execute('INSERT INTO protein_fail (name) VALUES(?)', (name))
+        cursor.execute('INSERT INTO protein_fail (name) VALUES(?)', [name])
 
     conn.commit()
 
     return pid
+
+
+
+
 
 
 def process_protein(conn, name):
@@ -99,6 +101,109 @@ def process_protein(conn, name):
     pid = get_protein_id(conn, name)
 
     print '{0}  has id {1}\n'.format(name, pid)
+
+    result_set = process_site(conn, pid)
+
+
+url_2 = 'http://www.phosphosite.org/proteinModificationSitesDomainsAction.action?id={0}&showAllSites=true'
+
+
+def secondLink(conn, pid, result_set):
+    f =  urllib2.urlopen(url_2.format(pid))
+    html_doc = f.read()
+
+    soup = bs4.BeautifulSoup(html_doc, 'html.parser')
+
+    target = soup.find_all('td', text='Sites Implicated In ')
+    if target:
+        target = target[0].parent.parent
+        target = target.contents[3].find_all('table')[0]
+        tds = target.find_all('td', attrs={"colspan": 3})
+        processSecondLinkTds(conn, pid, tds, result_set)
+
+def processSecondLinkTds(conn, pid, tds, result_set):
+    for td in tds:
+        anchors = td.find_all('a')
+        if anchors:
+            key = td.text.encode('ascii', 'ignore').lstrip()
+            colon = key.find(":")
+            if colon != -1:
+                key = key[0:colon]
+            else:
+                key = key.rstrip()
+            for a in anchors:
+                match = idPattern.search(str(a))
+                if match:
+                    site = a.text.encode('ascii', 'ignore').strip()
+                    put_site(conn, pid, key, site, match.group(1), result_set)
+
+
+def put_site(conn, pid, key, site, val, result_set):
+    sys.stderr.write('put_site(pid={0}, key={1}, site={2}, val={3})\n'.format(pid, key, site, val))
+
+def process_site(conn, pid):
+
+    cursor = conn.cursor()
+
+    #if is has failed previously skip it
+    cursor.execute('SELECT * FROM site_fail WHERE phosphosite_id=?', [str(pid)])
+    if cursor.fetchone() is not None:
+        sys.stderr.write('sites for {0} have already failed\n'.format(pid))
+        return None
+
+    #if it has succeeded previously skip it
+    cursor.execute('SELECT * FROM site_success WHERE phosphosite_id=?', [str(pid)])
+    if cursor.fetchone() is not None:
+        sys.stderr.write('sites for {0} have already succeeded\n'.format(pid))
+        return None
+
+    #no excuses better do it.
+    result_set = set()
+
+    secondLink(conn, pid, result_set)
+
+    sys.stderr.write('processing sites for {0}!\n'.format(pid))
+
+
+
+
+
+
+
+
+
+    conn.commit()
+
+    return result_set
+
+
+def get_site_name_id(conn, name):
+    cursor.execute('SELECT site_name_id FROM site_name WHERE name=?', [name])
+    snid = cursor.fetchone()
+    if snid is not None:
+        return int(snid)
+    cursor.execute('INSERT INTO site_name (name) VALUES(?)', [name])
+    return int(cursor.lastrowid)
+
+
+def get_site_category_id(conn, category):
+    cursor.execute('SELECT site_category_id FROM site_category WHERE category=?', [category])
+    scid = cursor.fetchone()
+    if scid is not None:
+        return int(scid)
+    cursor.execute('INSERT INTO site_category (category) VALUES(?)', [category])
+    return int(cursor.lastrowid)
+
+
+def get_control_name_id(conn, name):
+    cursor.execute('SELECT control_name_id FROM control_name WHERE name=?', [name])
+    snid = cursor.fetchone()
+    if snid is not None:
+        return int(snid)
+    cursor.execute('INSERT INTO control_name (name) VALUES(?)', [name])
+    return int(cursor.lastrowid)
+
+
 
 
 for protein in protein_list:
